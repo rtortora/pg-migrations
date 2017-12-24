b5846df04181eb0900a4463d5098d0657bbe748d
Richard Tortora <richard.tortora@gmail.com>
2017-12-24 01:50:54 -0500

    oops

--- src/commands/do_init.js
+++ src/commands/do_init.js
@@ -6,12 +6,12 @@ export default async function doInit(host) {
   const path = Path.join(host.rootPath, CONFIG_FILENAME);
   if (!(await FS.exists(path))) {
     await FS.writeFile(path, `export default {
-migrationsTableName: "migrations",
-migrationsPath: "./migrations/",
-getConnection: async ()=>{
-  const pg = /* do stuff to get a single pg client to your database */
-  return pg;
-}
+  migrationsTableName: "migrations",
+  migrationsPath: "./migrations/",
+  getConnection: async ()=>{
+    const pg = /* do stuff to get a single pg client to your database */
+    return pg;
+  }
 }
   `);
     console.log(`Wrote skeleton to ${path}`);
