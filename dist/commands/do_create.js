6c1e4d929f696182e4b7c7328537edbf8ec729d3
Richard Tortora <rick@petcarenow.com>
2019-07-30 14:42:05 -0400

    bumping dependencies

--- dist/commands/do_create.js
+++ dist/commands/do_create.js
@@ -1,4 +1,8 @@
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } } function _next(value) { step("next", value); } function _throw(err) { step("throw", err); } _next(); }); }; }
+"use strict";
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 const Path = require('path');
 
@@ -22,8 +26,10 @@ function () {
     console.log(`Wrote ${Path.join(config.migrationsPath, filename)}`);
   });
 
-  return function doCreate(_x, _x2) {
+  function doCreate(_x, _x2) {
     return _doCreate.apply(this, arguments);
-  };
+  }
+
+  return doCreate;
 }();
 //# sourceMappingURL=do_create.js.map
